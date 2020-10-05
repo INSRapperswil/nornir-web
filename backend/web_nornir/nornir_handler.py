4af8ffeaf2e778fd73403b0720fc376c6a26d3d5
Felix Kubli <felix.kubli@ost.ch>
2020-10-05 15:55:19 +0200

    add filter to task execution

--- backend/web_nornir/nornir_handler.py
+++ backend/web_nornir/nornir_handler.py
@@ -1,4 +1,5 @@
 from nornir import InitNornir
+from .job_discovery import get_job_function, get_job_definitions
 
 
 class NornirHandler:
@@ -11,5 +12,11 @@ class NornirHandler:
     def get_groups(self):
         return self.nr.inventory.groups
 
-    def execute_task(self, params: dict):
-        return self.nr.run(params)
+    @staticmethod
+    def get_job_template_definitions():
+        return get_job_definitions()
+
+    def execute_task(self, job_name: str, params: dict, filter_arguments: dict):
+        selection = self.nr.filter(filter_arguments)
+        params['task'] = get_job_function(job_name)
+        return selection.run(params)
