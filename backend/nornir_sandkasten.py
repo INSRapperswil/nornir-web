ffc6cd73c3821464cf7ba89e4feb1c95f8c1b691
Jerome Gygax <jerome.gygax@ost.ch>
2020-10-09 12:40:54 +0200

    nornir_handler returns full host information

--- backend/nornir_sandkasten.py
+++ backend/nornir_sandkasten.py
@@ -1,14 +1,18 @@
 from nornir import InitNornir
 
-nr = InitNornir(config_file='web_nornir/nornir_config/example_config/config.yaml')
 
-for host in nr.inventory.hosts:
-    print(host)
-    print(nr.inventory.hosts[host].keys())
-    print('---------------')
-    for key in nr.inventory.hosts[host].keys():
-        print(f'Key: {key}; Value: {nr.inventory.hosts[host][key]}')
-    print(nr.inventory.hosts[host].__getattribute__('port'))
-    print('\n\n--HOST--\n\n')
-    print(nr.inventory.hosts.get(host).get('hostname'))
-    print('\n\n--/HOST--\n\n')
+def main():
+    nr = InitNornir(config_file='web_nornir/nornir_config/example_config/config.yaml')
+    hosts: list = []
+    for host in nr.inventory.hosts:
+        host_data = nr.inventory.hosts[host].dict()
+        host_data.pop('connection_options', None)
+        host_data.pop('username', None)
+        host_data.pop('password', None)
+        hosts.append(host_data)
+
+    print(hosts)
+
+
+if __name__ == "__main__":
+    main()
