a1927946507a066bc3f1baa28eb400b6a03b1f91
Jerome Gygax <jerome.gygax@ost.ch>
2020-10-09 11:53:41 +0200

    Update create_groups.py

--- backend/api/management/commands/create_groups.py
+++ backend/api/management/commands/create_groups.py
@@ -1,15 +1,20 @@
 """
-Create permission groups
+Create permission groups.
 """
-
+from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 
 GROUPS = ['superuser', 'netadmin', 'support']
+MODELS = ContentType.objects.filter(app_label='api').all()
+PERMISSIONS = Permission.objects.filter(content_type__app_label='api').all()
 
 
 class Command(BaseCommand):
-    help = 'Creates read only default permission groups for users'
+    help = 'This script prepares the groups and permissions for Web-Nornir\n' \
+           '- Group "Superuser": Can read and write on all API nodes.\n' \
+           '- Group "Netadmin": Can read on all API nodes, can write on API node "task", "inventoryfilter".\n' \
+           '- Group "Support": Can only read on all API nodes.\n'
 
     def handle(self, *args, **options):
         for group in GROUPS:
