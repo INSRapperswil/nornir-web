e0f2ddc15cb6d7564d0cfa9f203d76811c58fa52
Jerome Gygax <jerome.gygax@ost.ch>
2020-10-09 17:53:43 +0200

    finalize testdb_setup

--- backend/api/management/commands/testdb_setup.py
+++ backend/api/management/commands/testdb_setup.py
@@ -13,26 +13,43 @@ class Command(BaseCommand):
     help = 'Setup DB with example data for tests'
 
     def handle(self, *args, **options):
-        # Create User
+
+        print('----    Creating Users    ----')
         User.objects.get_or_create(username='thomastest', password=make_password('imatestin'))
+        thomas = User.objects.get(username='thomastest')
 
-        user = User.objects.get(username='thomastest')
-        group = Group.objects.get(name='superuser')
-        group.user_set.add(user)
+        User.objects.get_or_create(username='norbert', password=make_password('netzwerk'))
+        norbert = User.objects.get(username='norbert')
 
-        # Create Inventory
+        User.objects.get_or_create(username='stefan', password=make_password('helldesk'))
+        stefan = User.objects.get(username='stefan')
+
+        superuser = Group.objects.get(name='superuser')
+        superuser.user_set.add(thomas)
+
+        netadmin = Group.objects.get(name='netadmin')
+        netadmin.user_set.add(norbert)
+
+        support = Group.objects.get(name='support')
+        support.user_set.add(stefan)
+
+        print('----    Creating Inventory    ----')
         models.Inventory.objects.create(name='Base', hosts_file='hosts.yml', groups_file='groups.yml', type=1)
 
-        # Create Template
+        print('----    Creating Job Templates    ----')
         models.JobTemplate.objects.create(name='Hello World', description='This prints a hello world',
                                           file_path='helo.py')
         models.JobTemplate.objects.create(name='Update Firmware', description='Updates Cisco Firmware',
                                           file_path='update_cisco.py')
 
-        # Create Task
-        models.Task.objects.create(name='Get Hello World', date_scheduled=datetime.utcnow(), created_by_id=1,
+        print('----    Creating Tasks    ----')
+        models.Task.objects.create(name='Get Hello World', date_scheduled=datetime.utcnow(), created_by_id=3,
                                    template_id=1)
+        models.Task.objects.create(name='Update Firmware', date_scheduled=datetime.utcnow(), created_by_id=2,
+                                   template_id=2)
 
-        # Create InventoryFilter
+        print('----    Creating Inventory Filters    ----')
         models.InventoryFilter.objects.create(inventory_id=1, task_id=2, filter='filter(site="cmh", role="host")')
         models.InventoryFilter.objects.create(inventory_id=1, task_id=1, filter='filter(platform="ios", role="host")')
+
+        print('----    ALL DONE!!    ----')
