37d9c4db8e553138abaedf52c592252d3cb89435
Jerome Gygax <jerome.gygax@ost.ch>
2020-10-21 12:52:37 +0200

    configure nornir dynamically

--- backend/nornir_sandkasten.py
+++ backend/nornir_sandkasten.py
@@ -1,16 +1,51 @@
 from nornir import InitNornir
 
-def main():
-    nr = InitNornir(config_file='web_nornir/nornir_config/example_config/config.yaml')
-    hosts: list = []
-    for host in nr.inventory.hosts:
-        host_data = nr.inventory.hosts[host].dict()
-        host_data.pop('connection_options', None)
-        host_data.pop('username', None)
-        host_data.pop('password', None)
-        hosts.append(host_data)
 
-    print(hosts)
+# TODO Config-Options
+# - Runner
+# - Num Workers
+# - Inventory Host/Group-File
+# - logging level
+# - logging file
+# - logging format
+
+def load_runner(num_workers):
+    if num_workers is None:
+        return None
+    else:
+        return {'plugin': 'threaded',
+                'options': {
+                    'num_workers': num_workers
+                }}
+
+
+def load_inventory(group_file, host_file):
+    if group_file is not None and host_file is not None:
+        return {'plugin': 'SimpleInventory',
+                'options': {
+                    'host_file': host_file,
+                    'group_file': group_file
+                }}
+
+
+def load_logging(level, file, format):
+    pass
+
+
+def main():
+    num_workers = None
+    # group_file = 'web_nornir/nornir_config/other_config/groups.yaml'
+    group_file = None
+    host_file = 'web_nornir/nornir_config/other_config/hosts.yaml'
+    nr = InitNornir(
+        config_file='web_nornir/nornir_config/example_config/config.yaml',
+        runner=load_runner(num_workers=num_workers),
+        inventory=load_inventory(group_file=group_file, host_file=host_file),
+
+    )
+    print(nr.config.runner.options['num_workers'])
+    print(nr.config.inventory.options['host_file'])
+    print(nr.config.inventory.options['group_file'])
 
 
 if __name__ == "__main__":
