e7c103752e13d3426b748e31900e5edff13f0384
Jerome Gygax <jerome.gygax@ost.ch>
2020-11-13 09:24:32 +0100

    fix result

--- backend/web_nornir/job_templates/say.py
+++ backend/web_nornir/job_templates/say.py
@@ -1,7 +1,8 @@
 from nornir.core.task import Task, Result
 
+
 def job_function(task: Task, text: str) -> Result:
     return Result(
         host=task.host,
-        result={ 'result': f"{task.host.name} says {text}" }
+        result=[Result(host=task.host, result=[f'{task.host.name} says {text}'])]
     )
