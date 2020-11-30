fa5d3c687646140b03f91a6ea2fa2ff0bde121c1
Felix Kubli <felix.kubli@renuo.ch>
2020-11-30 15:48:43 +0100

    remove more dead code

--- backend/web_nornir/job_discovery.py
+++ backend/web_nornir/job_discovery.py
@@ -10,16 +10,6 @@ class JobDiscovery:
         else:
             self.module_path = module
 
-    # obsolete?
-    def get_job_definitions(self):
-        job_definitions = []
-        for job in pkgutil.walk_packages([self.module_path]):
-            spec = importlib.util.spec_from_file_location(job.name, f'{self.module_path}/{job.name}.py')
-            task = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(task)
-            job_definitions.append(getattr(task, 'job_definition'))
-        return job_definitions
-
     def get_job_function(self, file_name: str, function_name: str):
         spec = importlib.util.spec_from_file_location(file_name, f'{self.module_path}/{file_name}')
         if spec:
